47f1616e4adf3b394b1857647ae4b25789d284ce
Rushikesh Bhoi <73032807+rushikeshbhoi@users.noreply.github.com>
2023-12-15 00:56:50 +0530

    Add files via upload

---
+++ Program180.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Program180
+{
+    public static void main(String arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iNo = 0;
+        int iMask = 0X00000004;
+        int iResult = 0;
+
+        System.out.println("Enter number : ");
+        iNo = sobj.nextInt();
+
+        iResult = iNo & iMask;
+        
+        if(iResult == iMask)
+        {
+            System.out.println("3rd bit is ON");
+        }
+        else
+        {
+            System.out.println("3rd bit is OFF");
+        }
+    }
+}
